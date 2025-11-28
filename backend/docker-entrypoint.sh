213d22fc84df347610781e222502fa9ea248b223
Krystian <krysquu@protonmail.com>
2025-11-28 20:14:54 +0100

    fix docker entrypoint /r sign

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 DB_HOST=${DB_HOST:-db}
 DB_PORT=${DB_PORT:-5432}
 
-
 until nc -z $DB_HOST $DB_PORT; do
   echo "Database is not avaiable yet."
   sleep 1
@@ -16,6 +15,5 @@ export FLASK_APP="run.py"
 
 flask db upgrade
 flask seed-demo
-
 
 exec "$@"
